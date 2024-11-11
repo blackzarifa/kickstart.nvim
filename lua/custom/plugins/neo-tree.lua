c22b380dfa768273ad5719fb99d336ea96b583b4
blackzarifa <rafaelirades@gmail.com>
2024-11-11 00:33:15 -0300

    indent and neo-tree plugins

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,24 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'Toggle NeoTree', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+      follow_current_file = true,
+    },
+  },
+}
