331f9558ab0e403fbc6261ee18f7d08bf782e63c
blackzarifa <rafaelirades@gmail.com>
2024-11-12 14:20:09 -0300

    Update lsp.lua

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -34,7 +34,6 @@ return {
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'rafamadriz/friendly-snippets',
       'onsails/lspkind.nvim',
