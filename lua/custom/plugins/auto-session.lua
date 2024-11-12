543cb5cf6cf7cc8d53eadc34cbeacaea5d8a6145
blackzarifa <rafaelirades@gmail.com>
2024-11-12 11:51:29 -0300

    add: auto-session

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,43 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+  config = function()
+    require('auto-session').setup {
+      log_level = 'error',
+      auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      auto_session_use_git_branch = true,
+      auto_session_enable_last_session = false,
+
+      -- Customize session name
+      auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
+      auto_session_enabled = true,
+      auto_session_create_enabled = true,
+
+      -- Advanced options
+      auto_restore_enabled = true,
+      auto_save_enabled = true,
+      auto_session_allow_dirs = {},
+
+      -- Hooks for pre/post session save/restore
+      pre_save_cmds = {
+        function()
+          -- Close all nvim-tree windows before saving
+          require('nvim-tree.api').tree.close()
+        end,
+        'NeoTreeClose',
+      },
+
+      -- Custom session name formatting
+      session_lens = {
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+    }
+
+    -- Keymaps for session management
+    vim.keymap.set('n', '<leader>ss', '<cmd>SessionSave<CR>', { desc = 'Save Session' })
+    vim.keymap.set('n', '<leader>sl', '<cmd>SessionLoad<CR>', { desc = 'Load Session' })
+    vim.keymap.set('n', '<leader>sd', '<cmd>SessionDelete<CR>', { desc = 'Delete Session' })
+  end,
+}
